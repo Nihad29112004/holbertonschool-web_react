978b2e5ee26f8b67b67e9a5e44a477226a9618d7
Nihad ALiyev <lnihad257@gmail.com>
2026-01-16 00:11:32 +0400

    react

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -1,10 +1,17 @@
-export const getCurrentYear = () => {
-  return new Date().getFullYear();
-};
+const getFullYear = () => {
+	let current_year = new Date().getFullYear();
+	return current_year;
+}
 
-export const getFooterCopy = (isIndex) => {
-  if (isIndex) {
-    return "Holberton School";
-  }
-  return "Holberton School main dashboard";
+const getFooterCopy = (isIndex) => {
+	if (isIndex === true) {
+		return "Holberton School";
+	} else {
+		return "Holberton School main dashboard";
+	}
+}
+
+module.exports = {
+	getFooterCopy,
+	getFullYear,
 };
