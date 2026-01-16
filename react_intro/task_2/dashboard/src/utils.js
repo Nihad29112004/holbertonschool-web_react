9ea041a411ad3a6077d4de6e3d1722448b556636
Nihad ALiyev <lnihad257@gmail.com>
2026-01-16 19:58:48 +0400

    Finish task 2: Login form added and lint passed

--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -1,9 +1,17 @@
-export const getCurrentYear = () => {
-  return new Date().getFullYear();
-};
-export const getFooterCopy = (isIndex) => {
-  if (isIndex) {
-    return 'Holberton School';
-  }
-  return 'Holberton School main dashboard';
-};
+// Return the current year
+export function getFullYear() {
+    return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+    if (isIndex) {
+        return 'Holberton School.';
+    }
+    else {
+        return 'Holberton School main dashboard';
+    }
+}
+
+export function getLatestNotification() {
+    return {__html: '<strong>Urgent requirement</strong> - complete by EOD'};
+}
