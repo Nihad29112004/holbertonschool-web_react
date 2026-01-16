7d435115b810c12e704c5f9a88a5d240b0719da9
Nihad ALiyev <lnihad257@gmail.com>
2026-01-16 20:14:41 +0400

    Update Notifications with button and list items

--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -1,17 +1,15 @@
-// Return the current year
-export function getFullYear() {
-    return new Date().getFullYear();
-}
+export const getCurrentYear = () => {
+  return new Date().getFullYear();
+};
 
-export function getFooterCopy(isIndex) {
-    if (isIndex) {
-        return 'Holberton School.';
-    }
-    else {
-        return 'Holberton School main dashboard';
-    }
-}
+export const getFooterCopy = (isIndex) => {
+  if (isIndex) {
+    return 'Holberton School';
+  }
+  return 'Holberton School main dashboard';
+};
 
-export function getLatestNotification() {
-    return {__html: '<strong>Urgent requirement</strong> - complete by EOD'};
-}
+// Yeni funksiya
+export const getLatestNotification = () => {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
+};
