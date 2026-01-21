30f5616ea48c54430e8232fe25c8c810fe52344a
Nihad ALiyev <lnihad257@gmail.com>
2026-01-21 19:26:41 +0400

    react component

--- react_component/task_0/dashboard/src/main.jsx
+++ react_component/task_0/dashboard/src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App/App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App/App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App isLoggedIn={false} />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App isLoggedIn={true}/>
+  </StrictMode>,
 )
