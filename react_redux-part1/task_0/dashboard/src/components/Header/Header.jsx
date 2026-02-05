a128bf154fa5ab48dbe5ca31b3b80dd61d5af064
Nihad ALiyev <lnihad257@gmail.com>
2026-02-05 16:43:50 +0400

    redux

--- react_redux-part1/task_0/dashboard/src/components/Header/Header.jsx
+++ react_redux-part1/task_0/dashboard/src/components/Header/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../features/auth/authSlice';
 import logo from '../../assets/holberton-logo.jpg';
 
 function Header() {
+  const user = useSelector((state) => state.auth.user);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+
   return (
-    <div className="header">
+    <header className="App-header">
       <img src={logo} alt="logo" />
       <h1>School dashboard</h1>
-    </div>
+      {isLoggedIn && (
+        <section id="logoutSection">
+          Welcome <b>{user?.email}</b> 
+          <a href="#" onClick={() => dispatch(logout())} style={{ marginLeft: '10px' }}>(logout)</a>
+        </section>
+      )}
+    </header>
   );
 }
 export default Header;
